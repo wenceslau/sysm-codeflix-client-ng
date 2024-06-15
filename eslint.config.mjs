ead3b015dec9a8e2efc87f8298f6cae684bd56ac
Wenceslau Neto <wbaneto@gmail.com>
2024-06-15 13:44:30 +0100

    feat: added eslint configuration and linting scripts, updated helper.txt.

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {languageOptions: {globals: globals.browser}},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "semi": [
+        "error",
+        "always"
+      ],
+      "quotes": [
+        "error",
+        "single"
+      ]
+    }
+  }
+];
